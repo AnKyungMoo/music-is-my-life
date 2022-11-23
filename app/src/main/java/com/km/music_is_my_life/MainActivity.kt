2e320156f15dbd0270ee4977e68b482d70b75f6a
AnKyungMoo <sodp2@naver.com>
2022-11-23 22:30:51 +0900

    init project

---
+++ app/src/main/java/com/km/music_is_my_life/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.km.music_is_my_life
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
