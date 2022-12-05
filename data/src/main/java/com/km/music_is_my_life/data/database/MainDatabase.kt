b30cc31b29e552f1698b52f08d4517568607b1d7
AnKyungMoo <sodp2@naver.com>
2022-12-05 20:39:05 +0900

    feat: #5 오류 안나게 임시 entity 추가

--- data/src/main/java/com/km/music_is_my_life/data/database/MainDatabase.kt
+++ data/src/main/java/com/km/music_is_my_life/data/database/MainDatabase.kt
@@ -2,8 +2,9 @@ package com.km.music_is_my_life.data.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.km.music_is_my_life.data.entity.TempEntity
 
-@Database(entities = [], version = 1)
+@Database(entities = [TempEntity::class], version = 1)
 abstract class MainDatabase : RoomDatabase() {
 
     companion object {
