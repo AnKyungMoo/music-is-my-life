a3e46b14b62602f11056c64e193eaca7605b3442
AnKyungMoo <sodp2@naver.com>
2023-01-06 19:38:07 +0900

    feat: #9 노래 검색 레이아웃 안의 버튼 기능 추가

--- presenter/src/main/java/com/km/music_is_my_life/presenter/ui/search/SearchSongActivity.kt
+++ presenter/src/main/java/com/km/music_is_my_life/presenter/ui/search/SearchSongActivity.kt
@@ -1,6 +1,7 @@
 package com.km.music_is_my_life.presenter.ui.search
 
 import android.os.Bundle
+import android.text.Editable
 import androidx.appcompat.app.AppCompatActivity
 import com.km.music_is_my_life.presenter.databinding.ActivitySearchSongBinding
 
@@ -12,5 +13,14 @@ class SearchSongActivity : AppCompatActivity() {
 
         binding = ActivitySearchSongBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        binding.btnBack.setOnClickListener { finish() }
+        binding.btnClearText.setOnClickListener {
+            binding.etSearchSong.text = Editable.Factory().newEditable("")
+        }
     }
 }
