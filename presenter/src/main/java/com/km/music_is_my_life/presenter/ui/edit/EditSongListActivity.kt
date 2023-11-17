fd18992f674561ac89880cafddfbd454f5f4a11a
AnKyungMoo <sodp2@naver.com>
2023-11-18 04:01:18 +0900

    refactor: 데이터바인딩 제거

---
+++ presenter/src/main/java/com/km/music_is_my_life/presenter/ui/edit/EditSongListActivity.kt
@@ -0,0 +1,15 @@
+package com.km.music_is_my_life.presenter.ui.edit
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.km.music_is_my_life.presenter.databinding.ActivityEditSongListBinding
+
+class EditSongListActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityEditSongListBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityEditSongListBinding.inflate(layoutInflater)
+    }
+}
