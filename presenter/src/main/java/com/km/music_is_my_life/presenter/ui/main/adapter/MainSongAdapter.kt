30a54fc1be597f198a8c10c576e426c8e526d11b
AnKyungMoo <sodp2@naver.com>
2023-11-17 23:22:15 +0900

    feat: #26 홈 노래아이템 디자인 수정

--- presenter/src/main/java/com/km/music_is_my_life/presenter/ui/main/adapter/MainSongAdapter.kt
+++ presenter/src/main/java/com/km/music_is_my_life/presenter/ui/main/adapter/MainSongAdapter.kt
@@ -1,20 +1,27 @@
 package com.km.music_is_my_life.presenter.ui.main.adapter
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.km.music_is_my_life.domain.model.SongGender
+import com.km.music_is_my_life.presenter.R
 import com.km.music_is_my_life.presenter.databinding.ItemSongBinding
 import com.km.music_is_my_life.presenter.ui.model.SongUiModel
+import kotlin.math.absoluteValue
 
 class MainSongAdapter :
     ListAdapter<SongUiModel, MainSongViewHolder>(MainSongViewHolder.diffUtil) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainSongViewHolder {
         return MainSongViewHolder(
-            ItemSongBinding.inflate(LayoutInflater.from(parent.context),
-            parent,
-            false)
+            ItemSongBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false,
+            )
         )
     }
 
@@ -28,6 +35,48 @@ class MainSongViewHolder(private val binding: ItemSongBinding) : RecyclerView.Vi
         binding.tvSongNumber.text = item.number
         binding.tvSongName.text = item.title
         binding.tvSinger.text = item.singer
+
+        bindSongGenderText(item.gender)
+        bindSongKeyViews(item.key)
+    }
+
+    private fun bindSongGenderText(gender: SongGender) {
+        when (gender) {
+            SongGender.MAN -> {
+                binding.tvGender.text = "남"
+                binding.tvGender.setTextColor(ContextCompat.getColor(binding.root.context, R.color.Text10))
+            }
+            SongGender.WOMAN -> {
+                binding.tvGender.text = "여"
+                binding.tvGender.setTextColor(ContextCompat.getColor(binding.root.context, R.color.Text11))
+            }
+        }
+    }
+
+    private fun bindSongKeyViews(key: Int) {
+        val (keyText, isPositiveNumber) = when (key) {
+            0 -> "원키" to null
+            else -> key.absoluteValue.toString() to (key > 0)
+        }
+
+        when (isPositiveNumber) {
+            null -> {
+                binding.ivKeyUpDownIcon.visibility = View.GONE
+                binding.tvSongKey.visibility = View.GONE
+                binding.tvOriginalKey.visibility = View.VISIBLE
+            }
+            else -> {
+                binding.ivKeyUpDownIcon.visibility = View.VISIBLE
+                binding.tvSongKey.visibility = View.VISIBLE
+                val keyUpDownIconRes = when (isPositiveNumber) {
+                    true -> R.drawable.ic_home_keyup
+                    false -> R.drawable.ic_home_keydown
+                }
+
+                binding.ivKeyUpDownIcon.setImageResource(keyUpDownIconRes)
+                binding.tvSongKey.text = keyText
+            }
+        }
     }
 
     companion object {
