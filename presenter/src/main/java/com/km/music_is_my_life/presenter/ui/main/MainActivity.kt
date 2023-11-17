abb44ea6637edeef746f7f1b45d1c1f1f9684e3d
AnKyungMoo <sodp2@naver.com>
2023-11-18 03:31:47 +0900

    feat: #29 메인 편집 버튼 팝업 추가

--- presenter/src/main/java/com/km/music_is_my_life/presenter/ui/main/MainActivity.kt
+++ presenter/src/main/java/com/km/music_is_my_life/presenter/ui/main/MainActivity.kt
@@ -2,10 +2,17 @@ package com.km.music_is_my_life.presenter.ui.main
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.Gravity
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.PopupWindow
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.tabs.TabLayoutMediator
+import com.km.music_is_my_life.presenter.R
 import com.km.music_is_my_life.presenter.databinding.ActivityMainBinding
+import com.km.music_is_my_life.presenter.ext.dp
 import com.km.music_is_my_life.presenter.ui.main.adapter.MainTabAdapter
 import com.km.music_is_my_life.presenter.ui.search.SearchSongActivity
 import dagger.hilt.android.AndroidEntryPoint
@@ -38,10 +45,36 @@ class MainActivity: AppCompatActivity() {
             startActivity(Intent(this, SearchSongActivity::class.java))
         }
         binding.viewPager.adapter = MainTabAdapter(this)
+        binding.btnEdit.setOnClickListener {
+            onClickEditButton()
+        }
 
         TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
             tab.text = tabTitles[position]
         }.attach()
+    }
+
+    private fun onClickEditButton() {
+        val view = layoutInflater.inflate(R.layout.group_popup_window, null)
+        val popupWidth = 86.dp(this)
+        val popupHeight = 90.dp(this)
+
+        val popupWindow = PopupWindow(
+            view,
+            popupWidth,
+            popupHeight,
+            true,
+        )
+
+        val location = IntArray(2)
+        binding.btnEdit.getLocationInWindow(location)
+
+        popupWindow.showAtLocation(
+            view,
+            Gravity.END,
+            18.dp(this),
+            -location[1] + 19.dp(this),
+        )
     }
 
 //    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
